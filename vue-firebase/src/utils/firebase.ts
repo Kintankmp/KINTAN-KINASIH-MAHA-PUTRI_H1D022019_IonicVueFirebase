f7d6e81c34a90788f2b8d5b4c4ce7d2fef7928f4
zzzAzazeX <kintan.mahaputri@mhs.unsoed.ac.id>
2024-11-25 18:12:57 +0700

    update

--- vue-firebase/src/utils/firebase.ts
+++ vue-firebase/src/utils/firebase.ts
@@ -1,18 +1,26 @@
 // src/utils/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDli3psmtts-IlbT0OQc8MKtrczpI8SZCE",
-  authDomain: "vue-firebase-bdc08.firebaseapp.com",
-  projectId: "vue-firebase-bdc08",
-  storageBucket: "vue-firebase-bdc08.firebasestorage.app",
-  messagingSenderId: "868280906799",
-  appId: "1:868280906799:web:b4512a079d1beccd9882da",
+    authDomain: "vue-firebase-bdc08.firebaseapp.com",
+    projectId: "vue-firebase-bdc08",
+    storageBucket: "vue-firebase-bdc08.firebasestorage.app",
+    messagingSenderId: "868280906799",
+    appId: "1:868280906799:web:b4512a079d1beccd9882da",
 };
 
+// Inisialisasi Firebase
 const firebase = initializeApp(firebaseConfig);
+
+// Konfigurasi Firebase Authentication
 const auth = getAuth(firebase);
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider };
+// Konfigurasi Firestore
+const db = getFirestore(firebase);
+
+// Ekspor modul
+export { auth, googleProvider, db };
